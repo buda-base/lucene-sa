d4664837e14048a2f0ad65bb392264806d661f5d
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-06-28 14:44:45 +0200

    quick attempt to implement syllabe tokenizer

--- src/main/java/io/bdrc/lucene/sa/SkrtSylTokenizer.java
+++ src/main/java/io/bdrc/lucene/sa/SkrtSylTokenizer.java
@@ -23,6 +23,8 @@ package io.bdrc.lucene.sa;
 import java.io.IOException;
 import java.util.HashMap;
 
+import org.apache.lucene.analysis.CharacterUtils;
+import org.apache.lucene.analysis.CharacterUtils.CharacterBuffer;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
@@ -37,127 +39,136 @@ import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
  * 
  */
 public final class SkrtSylTokenizer extends Tokenizer {
-  
+
 	/**
 	 * Construct a new TibSyllableTokenizer.
 	 */
 	public SkrtSylTokenizer() {
 	}
-	
+
 	private int offset = 0, bufferIndex = 0, dataLen = 0, finalOffset = 0;
-	private static final int MAX_WORD_LEN = 255;
+	private int previousChar = -1;
+	public static final int DEFAULT_MAX_WORD_LEN = 255;
 	private static final int IO_BUFFER_SIZE = 4096;
-	  
+	private final int maxTokenLen = 10;
+
+	public final static int VOWEL = 0;
+	public final static int MODIFIER = 1;
+	public final static int CONSONNANT = 2;
+	public final static int OTHER = 3;
+
 	private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
 	private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
 
-	private static final HashMap<Character, Character> charType = createMap();
-	private static final HashMap<Character, Character> createMap()
+	private final CharacterBuffer ioBuffer = CharacterUtils.newCharacterBuffer(IO_BUFFER_SIZE);
+
+
+	private static final HashMap<Integer, Integer> charType = createMap();
+	private static final HashMap<Integer, Integer> createMap()
 	{
-		HashMap<Character, Character> charType = new HashMap<Character, Character>();
+		HashMap<Integer, Integer> charType = new HashMap<>();
 		// vowels
-		charType.put('a', 'V');
-		charType.put('A', 'V');
-		charType.put('i', 'V');
-		charType.put('I', 'V');
-		charType.put('u', 'V');
-		charType.put('U', 'V');
-		charType.put('f', 'V');
-		charType.put('F', 'V');
-		charType.put('x', 'V');
-		charType.put('X', 'V');
-		charType.put('e', 'V');
-		charType.put('E', 'V');
-		charType.put('o', 'V');
-		charType.put('O', 'V');
+		charType.put((int)'a', VOWEL);
+		charType.put((int)'A', VOWEL);
+		charType.put((int)'i', VOWEL);
+		charType.put((int)'I', VOWEL);
+		charType.put((int)'u', VOWEL);
+		charType.put((int)'U', VOWEL);
+		charType.put((int)'f', VOWEL);
+		charType.put((int)'F', VOWEL);
+		charType.put((int)'x', VOWEL);
+		charType.put((int)'X', VOWEL);
+		charType.put((int)'e', VOWEL);
+		charType.put((int)'E', VOWEL);
+		charType.put((int)'o', VOWEL);
+		charType.put((int)'O', VOWEL);
 		// special class for anusvara & visarga, jihvamuliya, upadhmaniya
-		charType.put('M', 'X');
-		charType.put('H', 'X');
-		charType.put('V', 'X');
-		charType.put('Z', 'X');
+		charType.put((int)'M', MODIFIER);
+		charType.put((int)'H', MODIFIER);
+		charType.put((int)'V', MODIFIER);
+		charType.put((int)'Z', MODIFIER);
 		// consonants
-		charType.put('k', 'C');
-		charType.put('K', 'C');
-		charType.put('g', 'C');
-		charType.put('G', 'C');
-		charType.put('N', 'C');
-		charType.put('c', 'C');
-		charType.put('C', 'C');
-		charType.put('j', 'C');
-		charType.put('J', 'C');
-		charType.put('Y', 'C');
-		charType.put('w', 'C');
-		charType.put('W', 'C');
-		charType.put('q', 'C');
-		charType.put('Q', 'C');
-		charType.put('R', 'C');
-		charType.put('t', 'C');
-		charType.put('T', 'C');
-		charType.put('d', 'C');
-		charType.put('D', 'C');
-		charType.put('n', 'C');
-		charType.put('p', 'C');
-		charType.put('P', 'C');
-		charType.put('b', 'C');
-		charType.put('B', 'C');
-		charType.put('m', 'C');
-		charType.put('y', 'C');
-		charType.put('r', 'C');
-		charType.put('l', 'C');
-		charType.put('v', 'C');
-		charType.put('L', 'C');
-		charType.put('|', 'C');
-		charType.put('S', 'C');
-		charType.put('z', 'C');
-		charType.put('s', 'C');
-		charType.put('h', 'C');
+		charType.put((int)'k', CONSONNANT);
+		charType.put((int)'K', CONSONNANT);
+		charType.put((int)'g', CONSONNANT);
+		charType.put((int)'G', CONSONNANT);
+		charType.put((int)'N', CONSONNANT);
+		charType.put((int)'c', CONSONNANT);
+		charType.put((int)'C', CONSONNANT);
+		charType.put((int)'j', CONSONNANT);
+		charType.put((int)'J', CONSONNANT);
+		charType.put((int)'Y', CONSONNANT);
+		charType.put((int)'w', CONSONNANT);
+		charType.put((int)'W', CONSONNANT);
+		charType.put((int)'q', CONSONNANT);
+		charType.put((int)'Q', CONSONNANT);
+		charType.put((int)'R', CONSONNANT);
+		charType.put((int)'t', CONSONNANT);
+		charType.put((int)'T', CONSONNANT);
+		charType.put((int)'d', CONSONNANT);
+		charType.put((int)'D', CONSONNANT);
+		charType.put((int)'n', CONSONNANT);
+		charType.put((int)'p', CONSONNANT);
+		charType.put((int)'P', CONSONNANT);
+		charType.put((int)'b', CONSONNANT);
+		charType.put((int)'B', CONSONNANT);
+		charType.put((int)'m', CONSONNANT);
+		charType.put((int)'y', CONSONNANT);
+		charType.put((int)'r', CONSONNANT);
+		charType.put((int)'l', CONSONNANT);
+		charType.put((int)'v', CONSONNANT);
+		charType.put((int)'L', CONSONNANT);
+		charType.put((int)'|', CONSONNANT);
+		charType.put((int)'S', CONSONNANT);
+		charType.put((int)'z', CONSONNANT);
+		charType.put((int)'s', CONSONNANT);
+		charType.put((int)'h', CONSONNANT);
 		// Modifiers
-		charType.put('_', 'M');
-		charType.put('=', 'M');
-		charType.put('!', 'M');
-		charType.put('#', 'M');
-		charType.put('1', 'M');
-		charType.put('1', 'M');
-		charType.put('2', 'M');
-		charType.put('3', 'M');
-		charType.put('4', 'M');
-		charType.put('/', 'M');
-		charType.put('\\', 'M');
-		charType.put('^', 'M');
-		charType.put('6', 'M');
-		charType.put('7', 'M');
-		charType.put('8', 'M');
-		charType.put('9', 'M');
-		charType.put('+', 'M');
-		charType.put('~', 'M');
+		charType.put((int)'_', OTHER);
+		charType.put((int)'=', OTHER);
+		charType.put((int)'!', OTHER);
+		charType.put((int)'#', OTHER);
+		charType.put((int)'1', OTHER);
+		charType.put((int)'2', OTHER);
+		charType.put((int)'3', OTHER);
+		charType.put((int)'4', OTHER);
+		charType.put((int)'/', OTHER);
+		charType.put((int)'\\', OTHER);
+		charType.put((int)'^', OTHER);
+		charType.put((int)'6', OTHER);
+		charType.put((int)'7', OTHER);
+		charType.put((int)'8', OTHER);
+		charType.put((int)'9', OTHER);
+		charType.put((int)'+', OTHER);
+		charType.put((int)'~', OTHER);
 		return charType;
 	}
-		
-	public boolean isSylEnd(char char1, char char2) {
+
+	public boolean isSylEnd(int char1, int char2) {
 		/**
 		 * Returns true if a syllable ends between char1 and char2
 		 * @return
 		 */
-		// char1\char2 | nonSLP | M | C  | X | V |
-		//-------------|--------|---|----|---|---|
-		//    nonSLP   |   x    | x | x  | x | x |
-		//      M      |   A.   | x | B. | x | x |
-		//      C      |   A.   | x | x  | x | x |
-		//      X      |   A.   | x | C. | x | x |
-		//      V      |   A.   | x | D. | x | x |
-		//---------------------------------------
+		// char1\char2 | nonSLP | OTHER | CONSONNANT | MODIFIER | VOWEL |
+		//-------------|--------|-------|------------|----------|-------|
+		//    nonSLP   |   x    |   x   |     x      |    x     |   x   |
+		//      M      |   A.   |   x   |     B.     |    x     |   x   |
+		//      C      |   A.   |   x   |     x      |    x     |   x   |
+		//      X      |   A.   |   x   |     C.     |    x     |   x   |
+		//      V      |   A.   |   x   |     D.     |    x     |   x   |
+		//---------------------------------------------------------------
 		//
 		if (charType.containsKey(char1) && !charType.containsKey(char2)) {
 			// A.
 			return true;
-		} else if (charType.containsKey(char2) && charType.containsKey(char2) && charType.get(char2) == 'C') {
-			if (charType.containsKey(char1) && charType.get(char1) == 'M') {
+		} else if (charType.containsKey(char2) && charType.containsKey(char2) && charType.get(char2) == CONSONNANT) {
+			if (charType.containsKey(char1) && charType.get(char1) == OTHER) {
 				// B.
 				return true;
-			} else if (charType.containsKey(char1) && charType.get(char1) == 'X') {
+			} else if (charType.containsKey(char1) && charType.get(char1) == MODIFIER) {
 				// C.
 				return true;
-			} else if (charType.containsKey(char1) && charType.get(char1) == 'V') {
+			} else if (charType.containsKey(char1) && charType.get(char1) == VOWEL) {
 				// D.
 				return true;
 			} else {
@@ -167,43 +178,97 @@ public final class SkrtSylTokenizer extends Tokenizer {
 			return false;
 		}
 	}
-	public boolean isSylStart(char char1, char char2) {
+	public boolean isSylStart(int char1, int char2) {
 		/**
 		 * Returns true if a syllable starts between char1 and char2
 		 * @return
 		 */
-		// char1\char2 | nonSLP | M  |  C  | X  | V  |
-		//-------------|--------|----|-----|----|----|
-		//    nonSLP   |   x    | A. | A.  | A. | A. |
-		//      M      |   x    | x  | B.  | x  | x  |
-		//      C      |   x    | x  | x   | x  | x  |
-		//      X      |   x    | x  | B.  | x  | x  |
-		//      V      |   x    | x  | B.  | x  | x  |
-		//--------------------------------------------
+		// char1\char2 | nonSLP | OTHER | CONSONNANT | MODIFIER | VOWEL |
+		//-------------|--------|-------|------------|----------|-------|
+		//    nonSLP   |   x    |   A.  |     A.     |    A.    |   A.  |
+		//      M      |   x    |   x   |     B.     |    x     |   x   |
+		//      C      |   x    |   x   |     x      |    x     |   x   |
+		//      X      |   x    |   x   |     B.     |    x     |   x   |
+		//      V      |   x    |   x   |     B.     |    x     |   x   |
+		//---------------------------------------------------------------
 		//
 		if (!charType.containsKey(char1) && charType.containsKey(char2)) {
 			// A.
 			return true;
-		} else if ((charType.containsKey(char1) && charType.get(char1) != 'C') && (charType.containsKey(char2) && charType.get(char2) == 'C')) {
+		} else if ((charType.containsKey(char1) && charType.get(char1) != CONSONNANT) && (charType.containsKey(char2) && charType.get(char2) == CONSONNANT)) {
 			// B.
 			return true;
 		} else {
 			return false;
 		}
 	}
-  
+
+	protected boolean isTokenChar(int c) {
+		Integer res = charType.get(c);
+		return (res != null && res != OTHER); 
+	}
+
 	/** 
-	 * @return 
+	 * adapted from CharTokenizer
 	 */
 	@Override
 	public final boolean incrementToken() throws IOException {
 		clearAttributes();
+		int length = 0;
+		int start = -1; // this variable is always initialized
+		int end = -1;
 		char[] buffer = termAtt.buffer();
 		while (true) {
 			if (bufferIndex >= dataLen) {
 				offset += dataLen;
-				
+				CharacterUtils.fill(ioBuffer, input); // read supplementary char aware with CharacterUtils
+				if (ioBuffer.getLength() == 0) {
+					dataLen = 0; // so next offset += dataLen won't decrement offset
+					if (length > 0) {
+						break;
+					} else {
+						finalOffset = correctOffset(offset);
+						return false;
+					}
+				}
+				dataLen = ioBuffer.getLength();
+				bufferIndex = 0;
 			}
+			// use CharacterUtils here to support < 3.1 UTF-16 code unit behavior if the char based methods are gone
+			final int c = Character.codePointAt(ioBuffer.getBuffer(), bufferIndex, ioBuffer.getLength());
+			final int charCount = Character.charCount(c);
+			bufferIndex += charCount;
+
+			if (isTokenChar(c)) {               // if it's a token char
+				if (length == 0) {                // start of token
+					assert start == -1;
+					start = offset + bufferIndex - charCount;
+					end = start;
+				} else if (length >= buffer.length-1) { // check if a supplementary could run out of bounds
+					buffer = termAtt.resizeBuffer(2+length); // make sure a supplementary fits in the buffer
+				}
+				end += charCount;
+				length += Character.toChars(c, buffer, length); // buffer it
+				if (isSylStart(previousChar, c) || isSylEnd(previousChar, c)) {
+                    previousChar = c;
+                    break;
+				}
+				if (length >= maxTokenLen) { // buffer overflow! make sure to check for >= surrogate pair could break == test
+				    previousChar = c;
+					break;
+				}
+			} else if (length > 0) {           // at non-Letter w/ chars
+			    previousChar = c;
+				break;                           // return 'em
+			}
+			previousChar = c;
 		}
+
+		termAtt.setLength(length);
+		assert start != -1;
+		finalOffset = correctOffset(end);
+		offsetAtt.setOffset(correctOffset(start), finalOffset);
+		return true;
+
 	}
 }
