cf5ecca317c4ee2f5f5cb1a27243628640b8e092
drupchen <hhdrupchen@gmail.com>
2017-06-28 19:52:42 +0200

    amend merge

--- src/test/java/io/bdrc/lucene/sa/SanskritAnalyzerTest.java
+++ src/test/java/io/bdrc/lucene/sa/SanskritAnalyzerTest.java
@@ -85,6 +85,14 @@ public class SanskritAnalyzerTest
 	}
 	
 	@Test
+	public void testTransliterationFilter() throws Exception {
+	    CharFilter cs = new TransliterationFilter(new StringReader( "k" ) );
+	    TokenStream ts = tokenize(cs, new WhitespaceTokenizer());
+	    List<String> expected = Arrays.asList("k");
+	    assertTokenStream(ts, expected);
+	}
+	
+	@Test
 	public void isSylEndTest() throws IOException
 	{
 		System.out.println("Testing isSylEnd()");
@@ -195,32 +203,7 @@ public class SanskritAnalyzerTest
 		test.close();
 		assertThat(Arrays.asList(syl_boundary, no_syl_boundary), everyItem(is(true)));
 	}
-<<<<<<< HEAD
-		
-=======
-	public String unSandhiedTest = ".pfTivyA lABe pAlane ca yAvanty arTa.SAstrARi pUrva.AcAryEH prasTApitAni prAyaSas tAni saMhftya^ekam idam arTa.SAstraM kftam //";	
-	@Test
-	public void sylTokenizerTest() throws IOException
-	{
-		System.out.println("Testing SkrtSylTokenizer()");
-		String input = "AtmA atma";
-		Reader reader = new StringReader(input);
-		List<String> expected = Arrays.asList("A", "tmA", "a" ,"tma");
 
-		System.out.print(input + " => ");
-		TokenStream res = tokenize(reader, new SkrtSylTokenizer());
-		assertTokenStream(res, expected);
-	}
-	
-	@Test
-	public void testTransliterationFilter() throws Exception {
-	    CharFilter cs = new TransliterationFilter(new StringReader( "k" ) );
-	    TokenStream ts = tokenize(cs, new WhitespaceTokenizer());
-	    List<String> expected = Arrays.asList("k");
-	    assertTokenStream(ts, expected);
-	}
-
->>>>>>> branch 'master' of https://github.com/drupchen/lucene-sa.git
 	@AfterClass
 	public static void finish() {
 		System.out.println("after the test sequence");
