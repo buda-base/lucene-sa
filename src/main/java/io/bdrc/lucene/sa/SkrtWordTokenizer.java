ad5ae8c6c8899042eb382548bc8d4d9ceae024da
drupchen <hhdrupchen@gmail.com>
2017-08-08 17:59:21 +0200

    copy from TibWordTokenizer

---
+++ src/main/java/io/bdrc/lucene/sa/SkrtWordTokenizer.java
@@ -0,0 +1,300 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Buddhist Digital Resource Center (BDRC)
+ * 
+ * If this file is a derivation of another work the license header will appear 
+ * below; otherwise, this work is licensed under the Apache License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance with the 
+ * License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package io.bdrc.lucene.sa;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.CharacterUtils;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.CharacterUtils.CharacterBuffer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+
+import io.bdrc.lucene.stemmer.Optimizer;
+import io.bdrc.lucene.stemmer.Row;
+import io.bdrc.lucene.stemmer.Trie;
+
+/**
+ * A maximal-matching word tokenizer for Tibetan that uses a {@link Trie}.
+ * 
+ * <p>
+ * Takes a syllable at a time and returns the longest sequence of syllable that form a word within the Trie.<br>
+ * {@link #isTibLetter(int)} is used to distinguish clusters of letters forming syllables and {@code u\0F0B}(tsheg) to distinguish syllables within a word.
+ * <br> 
+ *  - Unknown syllables are tokenized as separate words.
+ * <br>
+ *  - All the punctuation is discarded from the produced tokens, including the tsheg that usually follows "ང". 
+ * <p>
+ * Due to its design, this tokenizer doesn't deal with contextual ambiguities.<br>
+ * For example, if both དོན and དོན་གྲུབ exist in the Trie, དོན་གྲུབ will be returned every time the sequence དོན + གྲུབ is found.<br>
+ * The sentence སེམས་ཅན་གྱི་དོན་གྲུབ་པར་ཤོག will be tokenized into "སེམས་ཅན + གྱི + དོན་གྲུབ + པར + ཤོག" (སེམས་ཅན + གྱི + དོན + གྲུབ་པར + ཤོག expected).   
+ * 
+ * Derived from Lucene 6.4.1 analysis.
+ * 
+ * @author Élie Roux
+ * @author Drupchen
+ *
+ */
+public final class SkrtWordTokenizer extends Tokenizer {
+	private Trie scanner;
+
+	// this tokenizer generates three attributes:
+	// term offset, positionIncrement and type
+	private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+	private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+	private boolean lemmatize = true;
+
+	/**
+	 * Constructs a TibWordTokenizer using the file designed by filename
+	 * @param filename
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public SkrtWordTokenizer(String filename) throws FileNotFoundException, IOException {
+		init(filename);
+	}
+
+	/**
+	 * Constructs a TibWordTokenizer using a default lexicon file (here "resource/output/total_lexicon.txt") 
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+	public SkrtWordTokenizer() throws FileNotFoundException, IOException {
+		init("resources/sanskrit-stemming-data/output/total_output.txt");
+	}
+
+	/**
+	 * Initializes and populates {@see #scanner} 
+	 * 
+	 * The format of each line in filename must be as follows: inflected-form + space + lemma
+	 * @param filename the file containing the entries to be added
+	 * @throws FileNotFoundException 
+	 * @throws IOException
+	 */
+	private void init(String filename) throws FileNotFoundException, IOException {
+		this.scanner = new Trie(true);
+
+		//		currently only adds the entries without any diff
+		try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				int spaceIndex = line.indexOf(' ');
+				if (spaceIndex == -1) {
+					throw new IllegalArgumentException("The dictionary file is corrupted in the following line.\n" + line);
+				} else {
+					this.scanner.add(line.substring(0, spaceIndex), line.substring(spaceIndex+1));
+				}
+			}
+			Optimizer opt = new Optimizer();
+			this.scanner.reduce(opt);
+		}
+	}
+
+	private int offset = 0, bufferIndex = 0, dataLen = 0, finalOffset = 0;
+	private static final int MAX_WORD_LEN = 255;
+	private static final int IO_BUFFER_SIZE = 4096;
+
+	//	  private final CharTermAttribute termAtt = addAttribute(CharTermAttribute.class);
+	//	  private final OffsetAttribute offsetAtt = addAttribute(OffsetAttribute.class);
+
+	private final CharacterBuffer ioBuffer = CharacterUtils.newCharacterBuffer(IO_BUFFER_SIZE);
+
+	/**
+	 * Called on each token character to normalize it before it is added to the
+	 * token. The default implementation does nothing. Subclasses may use this to,
+	 * e.g., lowercase tokens.
+	 */
+	protected int normalize(int c) {
+		return c;
+	}
+
+	@Override
+	public final boolean incrementToken() throws IOException {
+		//System.out.println("\nincrement token\n");
+		clearAttributes();
+		int length = 0;
+		int start = -1; // this variable is always initialized
+		int end = -1;
+		int confirmedEnd = -1;
+		int confirmedEndIndex = -1;
+		String cmd = null;
+		int w = -1;
+		int cmdIndex = -1;
+		int potentialEndCmdIndex = -1;
+		boolean potentialEnd = false;
+		boolean passedFirstSyllable = false;
+		Row now = null;
+		char[] buffer = termAtt.buffer();
+		while (true) {
+			if (bufferIndex >= dataLen) {
+				offset += dataLen;
+				CharacterUtils.fill(ioBuffer, input); // read supplementary char aware with CharacterUtils
+				if (ioBuffer.getLength() == 0) {
+					dataLen = 0; // so next offset += dataLen won't decrement offset
+					if (length > 0) {
+						break;
+					} else {
+						finalOffset = correctOffset(offset);
+						return false;
+					}
+				}
+				dataLen = ioBuffer.getLength();
+				bufferIndex = 0;
+			}
+			// use CharacterUtils here to support < 3.1 UTF-16 code unit behavior if the char based methods are gone
+			final int c = Character.codePointAt(ioBuffer.getBuffer(), bufferIndex, ioBuffer.getLength());
+			final int charCount = Character.charCount(c);
+			bufferIndex += charCount;
+
+			if (isSkrt(c)) {  // if it's a token char
+				if (length == 0) {                // start of token
+					assert(start == -1);
+					now = scanner.getRow(scanner.getRoot());
+					cmdIndex = now.getCmd((char) c);
+					potentialEnd = (cmdIndex >= 0); // we may have caught the end, but we must check if next character is a tsheg
+					if (potentialEnd) {
+						potentialEndCmdIndex = cmdIndex;
+					}
+					w = now.getRef((char) c);
+					now = (w >= 0) ? scanner.getRow(w) : null;
+					start = offset + bufferIndex - charCount;
+					end = start + charCount;
+				} else {
+					if (length >= buffer.length-1) { // check if a supplementary could run out of bounds
+						buffer = termAtt.resizeBuffer(2+length); // make sure a supplementary fits in the buffer
+					}
+					if (now == null) {
+						if (!passedFirstSyllable) {
+							// we're in a broken state (in the first syllable and no match)
+							// we just want to go to the end of the syllable
+							if (c == '\u0F0B') {
+								confirmedEnd = end;
+								confirmedEndIndex = bufferIndex;
+								break;
+							}
+							end += charCount; // else we're just passing
+						} else {
+							break;
+						}
+					} else {
+						if (c == '\u0F0B') {
+							passedFirstSyllable = true;
+							confirmedEnd = end;
+							confirmedEndIndex = bufferIndex;
+						}
+						end += charCount;
+						cmdIndex = now.getCmd((char) c);
+						potentialEnd = (cmdIndex >= 0); // we may have caught the end, but we must check if next character is a tsheg
+						if (potentialEnd) {
+							potentialEndCmdIndex = cmdIndex;
+						}
+						w = now.getRef((char) c);
+						now = (w >= 0) ? scanner.getRow(w) : null;
+					}
+				}
+				length += Character.toChars(normalize(c), buffer, length); // buffer it, normalized
+				if (length >= MAX_WORD_LEN) { // buffer overflow! make sure to check for >= surrogate pair could break == test
+					break;
+				}
+			} else if (length > 0) {           // at non-Letter w/ chars
+				break;                           // return 'em
+			}
+		}
+		if (potentialEnd) {
+			confirmedEnd = end;
+			confirmedEndIndex = bufferIndex;
+		}
+		if (confirmedEnd > 0) {
+			bufferIndex = confirmedEndIndex;
+			end = confirmedEnd;
+		}
+		
+		termAtt.setLength(end - start);
+		cmd = scanner.getCommandVal(potentialEndCmdIndex);
+		if (lemmatize && cmd != null) {
+			applyCmdToTermAtt(cmd);
+		}
+		assert(start != -1);
+		finalOffset = correctOffset(end);
+		offsetAtt.setOffset(correctOffset(start), finalOffset);
+		return true;
+	}
+
+	private boolean isSkrt(int c) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private void applyCmdToTermAtt(String cmd) {
+		if (cmd.charAt(0) == '>') {
+			// resize buffer
+			char operation = cmd.charAt(1);
+			switch(operation) {
+			case 'A':
+				termAtt.setLength(termAtt.length() - 1);
+				break;
+			case 'B':
+				termAtt.setLength(termAtt.length() - 2);
+				break;
+			case 'C':
+				termAtt.setLength(termAtt.length() - 3);
+				break;
+			case 'D':
+				// replaces the last character by a འ
+				char[] buffer = termAtt.buffer();
+				buffer[termAtt.length()-1] = 'འ';
+				break;
+			default:
+				throw new IllegalArgumentException("the operation should be A, B, C or D.");
+			}
+			
+		} else if (cmd.charAt(0) == '/') {
+			// replace content
+			termAtt.setEmpty().append(cmd.substring(1, cmd.length()));
+		} else {
+			
+		}
+
+	}
+
+	@Override
+	public final void end() throws IOException {
+		super.end();
+		// set final offset
+		offsetAtt.setOffset(finalOffset, finalOffset);
+	}
+
+	@Override
+	public void reset() throws IOException {
+		super.reset();
+		bufferIndex = 0;
+		offset = 0;
+		dataLen = 0;
+		finalOffset = 0;
+		ioBuffer.reset(); // make sure to reset the IO buffer!!
+	}
+
+	public void setLemmatize(boolean lemmatize) {
+		this.lemmatize = lemmatize;
+	}
+}
