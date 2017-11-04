b16baada133e5e3aada4ac8fb779a3f2637010a3
drupchen <hhdrupchen@gmail.com>
2017-11-04 15:41:48 +0100

    full fledged test

---
+++ src/test/java/io/bdrc/lucene/sa/FullTextParsingTest.java
@@ -0,0 +1,83 @@
+/*******************************************************************************
+ * Copyright (c) 2017 Buddhist Digital Resource Center (BDRC)
+ * 
+ * If this file is a derivation of another work the license header will appear 
+ * below; otherwise, this work is licensed under the Apache License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance with the 
+ * License.
+ * 
+ * You may obtain a copy of the License at
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package io.bdrc.lucene.sa;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.lucene.analysis.CharArraySet;
+import org.apache.lucene.analysis.CharFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+/**
+ * Unit tests for the Sanskrit tokenizers and filters.
+ */
+public class FullTextParsingTest
+{
+	static TokenStream tokenize(Reader reader, Tokenizer tokenizer) throws IOException {
+		tokenizer.close();
+		tokenizer.end();
+		tokenizer.setReader(reader);
+		tokenizer.reset();
+		return tokenizer;
+	}
+	
+	static private void printTokenStream(TokenStream tokenStream) {
+		try {
+			List<String> termList = new ArrayList<String>();
+			CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
+			while (tokenStream.incrementToken()) {
+				termList.add(charTermAttribute.toString());
+			}
+			System.out.println(String.join(" ", termList) + "\n");
+		} catch (IOException e) {
+			assertTrue(false);
+		}
+	}
+	
+    @Test
+    public void testDeva2SlpFilter() throws Exception {
+    	System.out.println("Testing transliterating from devanagari");
+    	Reader input = new FileReader("src/test/resources/tattvasangrahapanjika_raw_deva.txt");  
+    	CharFilter cs = new Deva2SlpFilter(input);
+    	System.out.println("0 " + input);
+		SkrtWordTokenizer skrtWordTokenizer = new SkrtWordTokenizer();
+		TokenStream words = tokenize(cs, skrtWordTokenizer);
+		CharArraySet stopSet = StopFilter.makeStopSet(SanskritAnalyzer.getWordList("src/main/resources/skrt-stopwords.txt", "#"));
+		TokenStream result = new StopFilter(words, stopSet);
+		printTokenStream(result);
+    }
+	
+	@AfterClass
+	public static void finish() {
+		System.out.println("after the test sequence");
+		System.out.println("Legend:\n0: input string\n1: expected output\n2: actual output\n");
+	}
+}
