d2b17ba6e811502543029ff387478f276a29743a
drupchen <hhdrupchen@gmail.com>
2017-12-01 12:54:00 +0100

    work in process

--- src/main/java/io/bdrc/lucene/sa/demo/PrettyPrintResult.java
+++ src/main/java/io/bdrc/lucene/sa/demo/PrettyPrintResult.java
@@ -31,7 +31,8 @@ public class PrettyPrintResult {
         Reader input = new StringReader(inputStr);
         CharFilter cs = new Deva2SlpFilter(input);
         TokenStream words = tokenize(cs, skrtWordTokenizer);
-        printProducedTokens(words, inputStr);
+        HashMap<String, int[]> terms = produceTerms(words);
+        String[] lines = createLines(terms, 10, inputStr);
     }
     
     static TokenStream tokenize(Reader reader, Tokenizer tokenizer) throws IOException {
@@ -42,22 +43,39 @@ public class PrettyPrintResult {
         return tokenizer;
     }
     
-    static private void printProducedTokens(TokenStream tokenStream, String inputStr) {
-        try {
-            HashMap<String, int[]> termList = new HashMap<String, int[]>();
+    static private String[] createLines(HashMap<String, int[]> terms, int termsPerLine, String inputStr) {
+        HashMap<String, HashMap<String, Integer>> lines = new HashMap<String, HashMap<String, Integer>>(); 
+        
+        int i = 0;
+        int batchSize = termsPerLine;
+        while (i <= terms.size()) {
+            if (i + batchSize > terms.size()) {
+                batchSize = terms.size() - i;
+            }
+            
+            int lineStart = 0;
+            int lineEnd = 0;
+            HashMap<String, Integer> batch = new HashMap<String, Integer>(); 
+            for (int t = i; t < i+batchSize; t++) {
+                
+            }
+            i += batchSize;
+        }
+        return null;
+    }
+    
+    static private HashMap<String, int[]> produceTerms(TokenStream tokenStream) throws IOException {
+            HashMap<String, int[]> terms = new HashMap<String, int[]>();
             CharTermAttribute TermAttr = tokenStream.addAttribute(CharTermAttribute.class);
             TypeAttribute typeAttr = tokenStream.addAttribute(TypeAttribute.class);
             OffsetAttribute offsetAttr = tokenStream.addAttribute(OffsetAttribute.class);
             while (tokenStream.incrementToken()) {
                 String token = TermAttr.toString();
-                int[] offsets = new int[]{offsetAttr.startOffset(), offsetAttr.endOffset()};
-                char type = typeAttr.type().equals("word") ? '✓': '❌';
-                termList.put(token+type, offsets);
+                int type = typeAttr.type().equals("word") ? 1: 0;//'✓': '❌';
+                int[] metadata = new int[]{type, offsetAttr.startOffset(), offsetAttr.endOffset()};
                 
-                System.out.println(TermAttr.toString() + type + offsets[0] + '/' + offsets[1]);
+                terms.put(token, metadata);
             }
-        } catch (IOException e) {
-            assertTrue(false);
-        }
-    }    
+            return terms;
+    }
 }
