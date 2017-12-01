adcef9e9baf6312b0dae72e2f22cd166e4ed7531
drupchen <hhdrupchen@gmail.com>
2017-12-01 12:08:05 +0100

    outputs token+type+offsets
    ex:
    tadA✓90575/90578
    iva✓90577/90579
    M❌90579/90580
    sanniveSa✓90581/90589

--- src/main/java/io/bdrc/lucene/sa/demo/PrettyPrintResult.java
+++ src/main/java/io/bdrc/lucene/sa/demo/PrettyPrintResult.java
@@ -6,13 +6,18 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import org.apache.lucene.analysis.CharFilter;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
 import org.apache.lucene.analysis.tokenattributes.TypeAttribute;
 
 import io.bdrc.lucene.sa.Deva2SlpFilter;
@@ -22,10 +27,11 @@ public class PrettyPrintResult {
     
     public static void main(String [] args) throws FileNotFoundException, IOException{
         SkrtWordTokenizer skrtWordTokenizer = new SkrtWordTokenizer();
-        Reader input = new FileReader("src/test/resources/tattvasangrahapanjika_raw_deva.txt");  
+        String inputStr = new String(Files.readAllBytes(Paths.get("src/test/resources/tattvasangrahapanjika_raw_deva.txt")));
+        Reader input = new StringReader(inputStr);
         CharFilter cs = new Deva2SlpFilter(input);
         TokenStream words = tokenize(cs, skrtWordTokenizer);
-        printProducedTokens(words);
+        printProducedTokens(words, inputStr);
     }
     
     static TokenStream tokenize(Reader reader, Tokenizer tokenizer) throws IOException {
@@ -36,15 +42,19 @@ public class PrettyPrintResult {
         return tokenizer;
     }
     
-    static private void printProducedTokens(TokenStream tokenStream) {
+    static private void printProducedTokens(TokenStream tokenStream, String inputStr) {
         try {
-            List<String> termList = new ArrayList<String>();
-            CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
-            TypeAttribute typeAttribute = tokenStream.addAttribute(TypeAttribute.class);
+            HashMap<String, int[]> termList = new HashMap<String, int[]>();
+            CharTermAttribute TermAttr = tokenStream.addAttribute(CharTermAttribute.class);
+            TypeAttribute typeAttr = tokenStream.addAttribute(TypeAttribute.class);
+            OffsetAttribute offsetAttr = tokenStream.addAttribute(OffsetAttribute.class);
             while (tokenStream.incrementToken()) {
-                termList.add(charTermAttribute.toString());
-                char type = typeAttribute.type().equals("word") ? '✓': '❌'; 
-                System.out.println(charTermAttribute.toString() + type);
+                String token = TermAttr.toString();
+                int[] offsets = new int[]{offsetAttr.startOffset(), offsetAttr.endOffset()};
+                char type = typeAttr.type().equals("word") ? '✓': '❌';
+                termList.put(token+type, offsets);
+                
+                System.out.println(TermAttr.toString() + type + offsets[0] + '/' + offsets[1]);
             }
         } catch (IOException e) {
             assertTrue(false);
